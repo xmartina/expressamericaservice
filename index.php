774ac76fedd25e577510a57e6f8e4e8a3c2fd2d5
unknown <martinsprince126@gmail.com>
2023-09-09 03:12:43 -0400

    init files

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ require_once ('config.php');
 $requestURI = $_SERVER['REQUEST_URI'];
 
 //router
-if ($requestURI == "/") {
-    require_once (WEB_URL.'/home.php');
-} elseif ($requestURI == "") {
-    require_once (WEB_URL.'/home.php');
-} else {
-    echo '404';
-}
+//if ($requestURI == "/") {
+//    require_once (WEB_URL.'/home.php');
+//} elseif ($requestURI == "") {
+//    require_once (WEB_URL.'/home.php');
+//} else {
+//    echo '404';
+//}
 //echo $requestURI;
