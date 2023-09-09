796b0b47b14a2da3b601cd8fb3d7a39fe392bfbd
unknown <martinsprince126@gmail.com>
2023-09-09 03:12:04 -0400

    init files

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ $requestURI = $_SERVER['REQUEST_URI'];
 
 //router
 if ($requestURI == "/") {
-  echo require_once (WEB_URL.'/home.php');
+    require_once (WEB_URL.'/home.php');
 } elseif ($requestURI == "") {
-    echo require_once (WEB_URL.'/home.php');
+    require_once (WEB_URL.'/home.php');
 } else {
     echo '404';
 }
